7b328da589f548bf4053addec6d3868fd4b28ce4
lilravx <0xg3n0@proton.me>
2025-09-30 17:43:37 -0400

    uploading sites

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,32 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1759268263',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/training/',
+      
+        '/about/',
+      
+        '/archives/',
+      
+        '/tags/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
