a4779709c46875c1370401ade30d0317f814cbf3
lilravx <0xg3n0@proton.me>
2025-10-07 15:18:35 -0400

    BITS update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1759864181',resources: [
+    cacheName: 'chirpy-1759864564',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
