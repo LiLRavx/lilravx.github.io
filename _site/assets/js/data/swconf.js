a0c2df8997dc70dca4b13a7d2d0eee5579cb0d7a
lilravx <0xg3n0@proton.me>
2025-10-17 09:45:11 -0400

    Uplload NamedPipe

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1759864564',resources: [
+    cacheName: 'chirpy-1760708600',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
